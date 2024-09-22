3c1dada8119c3e8afd3522eee5c37728e5b1848e
night-kriss <veseluhadima@gmail.com>
2024-09-22 20:11:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+var_int = 10
+var_float =8.4
+var_str="NO"
+var_int*=3.5
+big_int35=var_int
+var_float-=1
+var_int/var_float
+big_int35/var_float
+var_str=var_str*2
+var_str=var_str*2
+print(var_float,var_int, var_str, big_int35)
+var_str1 = "YES"
+var_str1*=3
+var_str3 = var_str+var_str1
+print(var_float,var_int, var_str3, big_int35)
